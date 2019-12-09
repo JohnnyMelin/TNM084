71613dc897bdfcbcfa793ceda476506e6598f437
johnnymelin <johnny.melin95@gmail.com>
2019-12-09 18:07:20 +0100

    master: adding git settings and new shape of noise particles

--- underwaterEnvironment/Assets/ParticleField.cs
+++ underwaterEnvironment/Assets/ParticleField.cs
@@ -35,10 +35,15 @@ public class ParticleField : MonoBehaviour
                 for (int z = 0; z < _gridSize.z; z++)
                 {
                     float noise = _fastNoise.GetSimplex(xOff + _offset.x, yOff + _offset.y, zOff + _offset.z) + 1;
-                    Gizmos.color = new Color(1, 1, 1, noise * noise * 0.5f);
+                    noise = noise / 2; // because the noise goes from -1 to 1 we first att 1 to make it go from0 to 2 and then divide by 2 for [0,1]
+
+                    float opacity = noise * noise;
+                    if (noise < 0.1) continue; // threshold the noise to only generate particles whre the noise is close to 1
+
+                    Gizmos.color = new Color(1, 1, 1, opacity);
                     Vector3 pos = new Vector3(x, y, z) + transform.position;
-                    Vector3 size = new Vector3(1, 1, 1);
-                    Gizmos.DrawCube(pos, size);
+                    //Vector3 size = new Vector3(0.1, 0.1, 0.1);
+                    Gizmos.DrawSphere(pos, 0.1f);
                     zOff += _increment;
                 }
                 yOff += _increment;
