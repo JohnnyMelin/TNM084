dc0b7a4295f699df703ea84f3d398cb14b56731f
johnnymelin <johnny.melin95@gmail.com>
2019-12-16 16:08:37 +0100

    noiseMovement: Added curl noise and animated it over time

--- underwaterEnvironment/Assets/ParticleField.cs
+++ underwaterEnvironment/Assets/ParticleField.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Unity.Mathematics;
 public class ParticleField : MonoBehaviour
 {
     FastNoise _fastNoise;
     public Vector3Int _gridSize;
     public float _increment;
     public float _particleFlow;
+    public float _waterTurbulence;
     public Vector3 _offset, _offsetSpeed;
     float time;
 
@@ -50,6 +51,27 @@ public class ParticleField : MonoBehaviour
         //}
     }
 
+    private float3 curlNoise(float3 p)
+    {
+        const float e = 0.1f;
+        float3 dx = new float3(e, 0.0f, 0.0f);
+        float3 dy = new float3(0.0f, e, 0.0f);
+        float3 dz = new float3(0.0f, 0.0f, e);
+
+        float3 p_x0 = _fastNoise.GetSimplex((p - dx).x, (p - dx).y, (p - dx).z);
+        float3 p_x1 = _fastNoise.GetSimplex((p + dx).x, (p + dx).y, (p + dx).z);
+        float3 p_y0 = _fastNoise.GetSimplex((p - dy).x, (p - dy).y, (p - dy).z);
+        float3 p_y1 = _fastNoise.GetSimplex((p + dy).x, (p + dy).y, (p + dy).z);
+        float3 p_z0 = _fastNoise.GetSimplex((p - dz).x,(p - dz).y,(p - dz).z);
+        float3 p_z1 = _fastNoise.GetSimplex((p + dz).x,(p + dz).y,(p + dz).z);
+
+        float x = p_y1.z - p_y0.z - p_z1.y + p_z0.y;
+        float y = p_z1.x - p_z0.x - p_x1.z + p_x0.z;
+        float z = p_x1.y - p_x0.y - p_y1.x + p_y0.x;
+
+        const float divisor = 1.0f / (2.0f * e);
+        return math.normalize(new float3(x, y, z) * divisor);
+    }
     private void OnDrawGizmos()
     {
         _fastNoise = new FastNoise();
@@ -73,9 +95,13 @@ public class ParticleField : MonoBehaviour
 
                     Gizmos.color = new Color(1, 1, 1, opacity);
                     Vector3 pos = new Vector3(x, y, z) + transform.position;
-                    // get a noise that can represent flownoise and then use the gradients to move pos to a new pos using gizmos draw line.
+                    Vector3 fieldPos = curlNoise(new float3(pos.x + Time.realtimeSinceStartup, pos.y, pos.z) * _waterTurbulence);
+                    Vector3 endPos = pos + (Vector3)math.normalize(fieldPos);
+                    //Vector3 endPos = pos + noiseFlow.normalized;
+                    // get a noise that can represent flownoise and then use the vectors to guide the particles in the field.
                     //Vector3 size = new Vector3(1, 1, 1);
-                    Gizmos.DrawSphere(pos, 0.1f);
+                    //Gizmos.DrawSphere(pos, 0.1f);
+                    Gizmos.DrawLine(pos,endPos);
                     //Gizmos.DrawCube(pos, size);
                     zOff += _increment;
                 }
