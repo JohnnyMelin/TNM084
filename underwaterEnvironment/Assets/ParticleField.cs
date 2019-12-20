79de9a1870a1b6bf57ab5feea9d76e5cfa3745e9
johnnymelin <johnny.melin95@gmail.com>
2019-12-20 12:42:51 +0100

    master: refactor

--- underwaterEnvironment/Assets/ParticleField.cs
+++ underwaterEnvironment/Assets/ParticleField.cs
@@ -27,10 +27,28 @@ public class ParticleField : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        updateParticles();
+        //int particlesAlive = particle_system.GetParticles(m_particle);
+        //_fastNoise = new FastNoise();
+        
+        //for(int i = 0; i < particlesAlive; i++)
+        //{
+        //    Vector3 pos = m_particle[i].position;   // get the position of the individual particle
+        //    Vector3 fieldPos = curlNoise(new float3(pos.x + Time.realtimeSinceStartup, pos.y, pos.z) * _waterTurbulence); // get the flowfield
+        //    Vector3 endPos = pos + (Vector3)math.normalize(fieldPos); // calculate the new direction for the particle based on the flowfield
+        //    m_particle[i].velocity += (endPos - pos) * m_drift; // apply the direction to the particle
+        //}
+
+        //// Apply the particle changes to the Particle System
+        //particle_system.SetParticles(m_particle, particlesAlive);
+    }
+
+    void updateParticles()
+    {
         int particlesAlive = particle_system.GetParticles(m_particle);
         _fastNoise = new FastNoise();
-        
-        for(int i = 0; i < particlesAlive; i++)
+
+        for (int i = 0; i < particlesAlive; i++)
         {
             Vector3 pos = m_particle[i].position;   // get the position of the individual particle
             Vector3 fieldPos = curlNoise(new float3(pos.x + Time.realtimeSinceStartup, pos.y, pos.z) * _waterTurbulence); // get the flowfield
@@ -40,40 +58,6 @@ public class ParticleField : MonoBehaviour
 
         // Apply the particle changes to the Particle System
         particle_system.SetParticles(m_particle, particlesAlive);
-        
-        //float xOff = 0;
-        //for (int x = 0; x < _gridSize.x; x++)
-        //{
-        //    float yOff = 0;
-        //    for (int y = 0; y < _gridSize.y; y++)
-        //    {
-        //        float zOff = 0;
-        //        for (int z = 0; z < _gridSize.z; z++)
-        //        {
-        //            Vector3 noiseFlow = new Vector3(Mathf.Cos(Time.realtimeSinceStartup), Mathf.Sin(Time.realtimeSinceStartup), Mathf.Cos(Time.realtimeSinceStartup));
-        //            noiseFlow *= _particleFlow;
-        //            float noise = _fastNoise.GetSimplex(xOff + _offset.x + noiseFlow.x, yOff + _offset.y + noiseFlow.y, zOff + _offset.z + noiseFlow.z) + 1;
-        //            noise = noise * 0.5f; // because the noise goes from -1 to 1 we first att 1 to make it go from0 to 2 and then divide by 2 for [0,1]
-
-        //            float opacity = noise * noise;
-        //            if (noise < 0.1) continue; // threshold the noise to only generate particles whre the noise is close to 1
-
-        //            //Gizmos.color = new Color(1, 1, 1, opacity);
-        //            Vector3 pos = new Vector3(x, y, z) + transform.position;
-        //            Vector3 fieldPos = curlNoise(new float3(pos.x + Time.realtimeSinceStartup, pos.y, pos.z) * _waterTurbulence);
-        //            Vector3 endPos = pos + (Vector3)math.normalize(fieldPos);
-        //            //Vector3 endPos = pos + noiseFlow.normalized;
-        //            // get a noise that can represent flownoise and then use the vectors to guide the particles in the field.
-        //            //Vector3 size = new Vector3(1, 1, 1);
-        //            //Gizmos.DrawSphere(pos, 0.1f);
-        //            //Gizmos.DrawLine(pos, endPos);
-        //            //Gizmos.DrawCube(pos, size);
-        //            zOff += _increment;
-        //        }
-        //        yOff += _increment;
-        //    }
-        //    xOff += _increment;
-        //}
     }
 
     private float3 curlNoise(float3 p)
